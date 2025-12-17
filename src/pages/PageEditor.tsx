1a9a47de6ddf6f665219762eb581ce03020bcddf
Midheal Bouchard <midhealbouchard@Mac.home.local>
2025-12-17 12:53:36 -0700

    fix: Remove unused getApiUrl import in PageEditor

--- src/pages/PageEditor.tsx
+++ src/pages/PageEditor.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { useParams } from 'react-router-dom';
-import { apiClient, getMediaUrl, getApiUrl } from '../services/apiClient';
+import { apiClient, getMediaUrl } from '../services/apiClient';
 import {
     Save,
     Image as ImageIcon,
